5e5832f129445c68b2ee8ff697b2e99f21c9ed79
ankit-gupta6791 <91ankitgupta@gmail.com>
2024-12-02 22:45:09 +0530

    updated compile sdk version to 35 to run the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "in.androaid.mvvmcomposeboiler"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "in.androaid.mvvmcomposeboiler"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
